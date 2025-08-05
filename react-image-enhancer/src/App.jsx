3ba9b1efc3effaad8357f94f32fe728f0091c7b2
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-08-05 13:14:21 +0530

    Add main application structure with Home component and basic layout

--- react-image-enhancer/src/App.jsx
+++ react-image-enhancer/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4'>
+      <div className='text-center mb-8'>
+        <h1 className='text-5xl font-bold text-gray-800 mb-2'>
+          AI Image Enhancer
+        </h1>
+        <p className='text-lg text-gray-500'>
+          Upload your image and let the AI do the magic!
+        </p>
+      </div>
+
+      <Home />
+      
+
+    </div>
   )
 }
 
---
+++ react-image-enhancer/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import ImagePreview from "./ImagePreview";
+import ImageUpload from "./ImageUpload";
+const Home = () => {
+  return (
+    <div>
+      <ImagePreview/>
+      <ImageUpload/>
+    </div>
+  )
+}
+
+export default Home
