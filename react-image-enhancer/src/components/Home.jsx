8e11f583476138771127a7467c63c18004d807f0
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-08-05 16:13:08 +0530

    Add image enhancement functionality with API integration
    - Implemented image upload and enhancement logic in Home component
    - Created enhancedImageAPI utility for handling API requests
    - Updated ImageUpload and ImagePreview components for new functionality
    - Added loading state management during image processing
    - Included axios and dotenv as dependencies

--- react-image-enhancer/src/components/Home.jsx
+++ react-image-enhancer/src/components/Home.jsx
@@ -1,12 +1,36 @@
-import ImagePreview from "./ImagePreview";
 import ImageUpload from "./ImageUpload";
-const Home = () => {
-  return (
-    <div>
-      <ImagePreview/>
-      <ImageUpload/>
-    </div>
-  )
-}
+import ImagePreview from "./ImagePreview";
+import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhanceImageApi";
 
-export default Home
+const Home = () => {
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setloading] = useState(false);
+
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setloading(true);
+        try {
+            const enhancedURL = await enhancedImageAPI(file);
+            setEnhancedImage(enhancedURL);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try again later.");
+        }
+    };
+
+    return (
+        <>
+            <ImageUpload UploadImageHandler={UploadImageHandler} />
+            <ImagePreview
+                loading={loading}
+                uploaded={uploadImage}
+                enhanced={enhancedImage?.image}
+            />
+        </>
+    );
+};
+
+export default Home;
--- react-image-enhancer/src/components/ImageUpload.jsx
+++ react-image-enhancer/src/components/ImageUpload.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
 
-const ImageUpload = () => {
+
+const ImageUpload = (props) => {
+
+
+  const ShowImageHandler = (e) => {
+    const file = e.target.files[0];
+    if(file){
+      props.UploadImageHandler(file);
+    }
+  }
+
   return (
-    <div>ImageUpload</div>
+    <div className='bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl'>
+      <label 
+        htmlFor="fileInput" 
+        className='block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center hover:border-blue-400 transition-all'>
+        
+        <input type="file" id='fileInput' className='hidden' onChange={ShowImageHandler}/> 
+        <span className='text-lg font-medium text-gray-600'>
+          Upload your Image
+        </span>
+      </label>
+
+    </div>
   )
 }
 
